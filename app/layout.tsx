29fc72adaac337030183bb418d21a5d9d116e8b0
koala819 <x.genolhac@gmail.com>
2023-10-10 09:57:39 +0200

    my first commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Top } from "@/components/Top";
+import { Footer } from "@/components/Footer";
+import { Providers } from "./providers";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DIX31",
+  description: "Developpeur Independant Xavier",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='fr'>
+      <body className={`${inter.className}`}>
+        <Providers>
+          <div className='min-h-screen min-w-screen'>
+            <div className=' flex flex-col h-screen'>
+              <Top />
+              <main className='flex-1'>
+                <div className='w-full lg:w-8/12 px-4 mx-auto mt-6'>
+                  <div className='flex flex-col min-w-0 break-words w-full mb-6rounded-lg bg-gray-50 dark:bg-slate-800 border-0'>
+                    <GoogleAnalytics />
+                    {children}
+                  </div>
+                </div>
+              </main>
+              <Footer />
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { AiFillChrome } from "react-icons/ai";
+import { LuCopyleft } from "react-icons/lu";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { Tooltip } from "@nextui-org/react";
+
+export function Footer() {
+  return (
+    <footer className='text-gray-900 bg-gray-200 dark:bg-slate-700 dark:text-slate-300'>
+      <div className='container px-5 md:py-12 mx-auto flex md:items-center md:flex-row md:flex-nowrap flex-wrap flex-col'>
+        <div className='flex w-full md:w-1/3 px-4 md:justify-start justify-center mt-4 md:mt-0'>
+          <nav className='flex flex-col mb-8'>
+            <h2 className='title-font font-medium tracking-widest text-sm mb-3 uppercase'>
+              Découvrez davantage
+            </h2>
+            <Link
+              href='#'
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              Item_1
+            </Link>
+            <Link
+              href='#'
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              Item_2
+            </Link>
+            <Link
+              href='#'
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              Item_3
+            </Link>
+          </nav>
+        </div>
+
+        <section className='w-full md:w-1/3 flex justify-center items-center'>
+          <div className='md:w-11/12 w-full flex-shrink-0 md:mx-0 mx-auto text-center md:text-left'>
+            <div className='flex title-font font-medium items-center justify-center'>
+              <AiFillChrome size={42} />
+              <span className='ml-3 text-xl'>TITLE</span>
+            </div>
+          </div>
+        </section>
+
+        <div className='flex w-full md:w-1/3 px-4 md:justify-end justify-center mt-4 md:mt-0'>
+          <nav className='flex flex-col mb-8'>
+            <h2 className='title-font font-medium tracking-widest text-sm mb-3 uppercase'>
+              Liens utiles
+            </h2>
+
+            <Link
+              href='/mentions'
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              mentions légales
+            </Link>
+            <Link
+              href='/protection'
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              protections des données
+            </Link>
+            <Link
+              href=''
+              className='text-gray-600 hover:underline dark:text-zinc-300 capitalize'
+            >
+              contact
+            </Link>
+          </nav>
+        </div>
+      </div>
+      <div className='bg-gray-100 dark:bg-black'>
+        <div className='container mx-auto py-4 px-5 flex flex-wrap flex-row'>
+          <Link
+            href='https://fr.wikipedia.org/wiki/Copyleft'
+            target='_blank'
+            className='flex text-gray-500 items-center space-x-2 dark:text-gray-200 hover:font-bold'
+          >
+            <p>Copyleft</p>
+            <LuCopyleft />
+            <p> 2023 by Xavier</p>
+          </Link>
+          <div className='ml-auto'>
+            <span className='inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start space-x-8'>
+              <Tooltip showArrow={true} color='primary' content='Linkedin'>
+                <Link
+                  href='https://www.linkedin.com/in/xavier%F0%9F%92%BB-genolhac-79a98390/'
+                  target='_blank'
+                  className='text-gray-500 dark:text-gray-200 dark:hover:text-blue-500'
+                >
+                  <FaLinkedin size={24} />
+                </Link>
+              </Tooltip>
+              <Tooltip showArrow={true} color='default' content='Github'>
+                <Link
+                  href='https://github.com/koala819/'
+                  target='_blank'
+                  className='ml-3 text-gray-500 dark:text-gray-200 dark:hover:text-yellow-500'
+                >
+                  <FaGithub size={24} />
+                </Link>
+              </Tooltip>
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
