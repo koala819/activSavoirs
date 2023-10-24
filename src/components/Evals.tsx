4f43221aa2ebc649895f85f46fa506bfc0b6e4e6
koala819 <x.genolhac@gmail.com>
2023-10-24 12:25:25 +0200

    Eval atom

--- src/components/Evals.tsx
+++ src/components/Evals.tsx
@@ -1,8 +1,5 @@
 "use client";
 import { Chip } from "@nextui-org/react";
-import Image from "next/image";
-import { format } from "date-fns";
-import { fr } from "date-fns/locale";
 import { useState } from "react";
 import {
   FaChartBar,
@@ -14,10 +11,10 @@ import {
   FaLightbulb,
   FaPhone,
   FaShoppingCart,
-  FaStar,
   FaTrophy,
   FaUsers,
 } from "react-icons/fa";
+import { Eval } from "@/src/components/atoms/Eval";
 
 export function Evals({ evals }: { evals: any }) {
   const [selectedTag, setSelectedTag] = useState<any | null>(null);
@@ -122,48 +119,7 @@ export function Evals({ evals }: { evals: any }) {
 
           return (
             <div key={index} className='flex items-center justify-center'>
-              <section
-                key={index}
-                className='max-w-md py-4 px-2 bg-white dark:bg-slate-600 shadow-lg rounded-lg '
-              >
-                <div className='flex items-center mb-4'>
-                  <div className='w-full'>
-                    <Image
-                      width={100}
-                      height={100}
-                      alt={evaluation.data.logo.alt}
-                      src={evaluation.data.logo.url}
-                    />
-                  </div>
-
-                  <div className='flex justify-end w-full'>
-                    <h2 className='text-gray-800 dark:text-sky-500 text-xl font-semibold'>
-                      {evaluation.data.prenom}
-                    </h2>
-                  </div>
-                </div>
-                <div className='flex  mb-4'>
-                  {Array.from({ length: evaluation.data.note }).map(
-                    (_, index) => (
-                      <FaStar
-                        key={index}
-                        size={24}
-                        className='text-yellow-300'
-                      />
-                    )
-                  )}
-                </div>
-                <div>
-                  <p className='mt-2 text-gray-600 dark:text-gray-200'>
-                    {evaluation.data.avis[0].text}
-                  </p>
-                </div>
-                <div className='flex mt-4'>
-                  {format(new Date(evaluation.data.date), "dd LLLL yyyy", {
-                    locale: fr,
-                  })}
-                </div>
-              </section>
+              <Eval evaluation={evaluation} />
             </div>
           );
         })}
---
+++ src/components/atoms/Eval.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Image from "next/image";
+import { FaStar } from "react-icons/fa";
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
+
+export function Eval({ evaluation }: { evaluation: any }) {
+  return (
+    <section className='max-w-md py-4 px-2 bg-white dark:bg-slate-600 shadow-lg rounded-lg '>
+      <div className='flex items-center mb-4'>
+        <div className='w-full'>
+          <Image
+            width={100}
+            height={100}
+            alt={evaluation.data.logo.alt}
+            src={evaluation.data.logo.url}
+          />
+        </div>
+
+        <div className='flex justify-end w-full'>
+          <h2 className='text-gray-800 dark:text-sky-500 text-xl font-semibold'>
+            {evaluation.data.prenom}
+          </h2>
+        </div>
+      </div>
+      <div className='flex  mb-4'>
+        {Array.from({ length: evaluation.data.note }).map((_, index) => (
+          <FaStar key={index} size={24} className='text-yellow-300' />
+        ))}
+      </div>
+      <div>
+        <p className='mt-2 text-gray-600 dark:text-gray-200'>
+          {evaluation.data.avis[0].text}
+        </p>
+      </div>
+      <div className='flex mt-4'>
+        {format(new Date(evaluation.data.date), "dd LLLL yyyy", {
+          locale: fr,
+        })}
+      </div>
+    </section>
+  );
+}
