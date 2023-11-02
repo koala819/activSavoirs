f6a49e3475e660302e8922ce781a8de467b900ad
koala819 <x.genolhac@gmail.com>
2023-11-02 09:05:50 +0100

    fix display evals in Thematiques

--- src/app/thematiques/page.tsx
+++ src/app/thematiques/page.tsx
@@ -1,25 +1,12 @@
 import { createClient } from "@/prismicio";
 import { notFound } from "next/navigation";
 import { Thematiques } from "@/src/components/templates/Thematiques";
-// import { thematiques } from "@/src/types/thematiques";
 
 export default async function Page() {
   const client = createClient();
   const evals = await client
     .getByType("evals", { pageSize: 1000, page: 1 })
     .catch(() => notFound());
-
-  // const tagToFilter = thematiques.efficaciteManageriale?.advices?.tag;
-  // const evalsWithTag = evals.results.filter((evaluation) => {
-  //   return evaluation.tags[0] === tagToFilter;
-  // });
-
-  // const displayEvals: any[] = [];
-  // evalsWithTag.map((evaluation) => {
-  //   displayEvals.push(evaluation);
-  // });
-
-  // console.log("displayEvals", displayEvals);
 
   return <Thematiques evaluations={evals} />;
 }
--- src/components/templates/Thematiques.tsx
+++ src/components/templates/Thematiques.tsx
@@ -5,7 +5,8 @@ import { thematiques } from "@/src/types/thematiques";
 import { Button } from "@nextui-org/react";
 
 export function Thematiques({ evaluations }: any) {
-  const [index, setIndex] = useState(0);
+  const [indexTheme, setIndexTheme] = useState(0);
+
   const menu = [
     { text: "thématiques abordées" },
     { text: "supports" },
@@ -13,23 +14,35 @@ export function Thematiques({ evaluations }: any) {
     { text: "avis" },
   ];
 
-  const theme = thematiques.map((thematique) => thematique.title);
+  const themes = thematiques.map((thematique) => thematique.title);
 
   function handlePrevious() {
-    if (index === 0) {
-      setIndex(theme.length - 1);
+    if (indexTheme === 0) {
+      setIndexTheme(themes.length - 1);
     } else {
-      setIndex(index - 1);
+      setIndexTheme(indexTheme - 1);
     }
   }
 
   function handleNext() {
-    if (index === theme.length - 1) {
-      setIndex(0);
+    if (indexTheme === themes.length - 1) {
+      setIndexTheme(0);
     } else {
-      setIndex(index + 1);
+      setIndexTheme(indexTheme + 1);
     }
   }
+
+  const tagToFilter = thematiques[indexTheme].advices?.tag;
+  const evalsWithTag = evaluations.results.filter((evaluation: any) => {
+    return evaluation.tags[0] === tagToFilter;
+  });
+
+  const displayEvals: any[] = [];
+  evalsWithTag.map((evaluation: any) => {
+    displayEvals.push(evaluation);
+  });
+
+  // console.log("displayEvals", displayEvals);
 
   return (
     <div className='flex'>
@@ -40,8 +53,8 @@ export function Thematiques({ evaluations }: any) {
       </aside>
       <ThematiqueFormation
         menu={menu}
-        evals={evaluations}
-        selected={thematiques[index]}
+        evals={displayEvals}
+        selected={thematiques[indexTheme]}
       />
       <aside>
         <Button color='default' variant='light' onClick={handleNext}>
