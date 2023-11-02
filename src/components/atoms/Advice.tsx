42076e41e781cef604b56672ff921f969ded9c51
koala819 <x.genolhac@gmail.com>
2023-11-02 20:24:22 +0100

    styles Advices

--- src/components/atoms/Advice.tsx
+++ src/components/atoms/Advice.tsx
@@ -2,7 +2,7 @@ import { Review } from "@/src/components/atoms/Review";
 
 export function Advice({ evals }: any) {
   return (
-    <div className='grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
       {evals.map((evaluation: any, index: number) => {
         return <Review evaluation={evaluation} key={index} />;
       })}
