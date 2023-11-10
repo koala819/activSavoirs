eb47396ef0fbbcaaf1c2379ffa49d8c788d38b88
koala819 <x.genolhac@gmail.com>
2023-11-10 10:55:40 +0100

    add metada in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import { createClient } from "@/prismicio";
 import { notFound } from "next/navigation";
 import { Homepage } from "@/src/components/templates/HomePage";
 import { Suspense } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Activ'Savoirs - Activateur de Compétences pour Entreprises Agile",
+  description:
+    "Renforcez la compétitivité de votre entreprise avec Activ'Savoirs. Nous proposons des actions de formation ciblées pour améliorer l'efficacité et l'agilité de vos équipes. Explorez nos solutions de montée en compétences.",
+};
+
 export default async function Page() {
   const client = createClient();
   const accueil = await client.getSingle("accueil").catch(() => notFound());
