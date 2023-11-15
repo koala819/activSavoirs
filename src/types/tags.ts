fd7bff5da7afc871ea8b61ca4f621e77393958a7
koala819 <x.genolhac@gmail.com>
2023-11-15 11:58:35 +0100

    new Evals : loi finances 2022

--- src/types/tags.ts
+++ src/types/tags.ts
@@ -1,4 +1,5 @@
 import {
+  FaBalanceScale,
   FaChartBar,
   FaChartLine,
   FaBookDead,
@@ -14,33 +15,37 @@ import {
 export const tags = [
   { name: "Développer ses compétences achat", icon: FaShoppingCart },
   {
-    name: "Maîtriser la prospection immobilière de terrain",
-    icon: FaHome,
-  },
-  { name: "Piges et prospection téléphonique", icon: FaPhone },
-  {
-    name: "Savoir vendre avec efficacité le mandat exclusif",
-    icon: FaFileContract,
-  },
-  {
-    name: "Maîtriser les fondamentaux du management d'une direction régionale",
-    icon: FaUsers,
-  },
-  {
-    name: "Savoir optimiser son efficacité commerciale",
-    icon: FaChartLine,
-  },
-  {
-    name: "Les fondamentaux de la performance commerciale",
-    icon: FaTrophy,
+    name: "Actualisation loi finances 2022",
+    icon: FaBalanceScale,
   },
   {
     name: "La performance commerciale",
     icon: FaChartBar,
   },
   {
+    name: "Les fondamentaux de la performance commerciale",
+    icon: FaTrophy,
+  },
+  {
+    name: "Maîtriser les fondamentaux du management d'une direction régionale",
+    icon: FaUsers,
+  },
+  {
+    name: "Maîtriser la prospection immobilière de terrain",
+    icon: FaHome,
+  },
+  { name: "Piges et prospection téléphonique", icon: FaPhone },
+  {
     name: "Savoir développer son efficacité commerciale",
     icon: FaLightbulb,
+  },
+  {
+    name: "Savoir optimiser son efficacité commerciale",
+    icon: FaChartLine,
+  },
+  {
+    name: "Savoir vendre avec efficacité le mandat exclusif",
+    icon: FaFileContract,
   },
   {
     name: undefined,
