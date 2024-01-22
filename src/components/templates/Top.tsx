637fa23214c242cf8c5d345078f14012e8daaf7c
koala819 <x.genolhac@gmail.com>
2024-01-22 15:10:15 +0100

    increase logo size

--- src/components/templates/Top.tsx
+++ src/components/templates/Top.tsx
@@ -1,6 +1,12 @@
-"use client";
-import { useState } from "react";
+'use client'
+
 import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Link,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -8,115 +14,114 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
-  Link,
-  DropdownItem,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  Button,
-} from "@nextui-org/react";
-import { usePathname } from "next/navigation";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useTheme } from "next-themes";
-import whitelogo from "@/public/images/logo.png";
-import blackLogo from "@/public/images/logoFooter.png";
-import { thematics } from "@/src/types/thematics";
-import { menuItems } from "@/src/types/models";
-import { FiChevronDown } from "react-icons/fi";
-import { colorVariants } from "@/src/lib/colorVariants";
+} from '@nextui-org/react'
+import { useState } from 'react'
+import { FiChevronDown } from 'react-icons/fi'
+
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+
+import { menuItems } from '@/src/types/models'
+import { thematics } from '@/src/types/thematics'
+
+import whitelogo from '@/public/images/logo.png'
+import { colorVariants } from '@/src/lib/colorVariants'
 
 export function Top() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const router = useRouter();
-  const path = usePathname();
-  const { resolvedTheme } = useTheme();
-
-  const logo = resolvedTheme === "dark" ? blackLogo : whitelogo;
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const router = useRouter()
+  const path = usePathname()
 
   const menuItems: menuItems[] = [
-    { name: "Accueil", path: "/" },
-    { name: "Nos Thématiques" },
+    { name: 'Accueil', path: '/' },
+    { name: 'Nos Thématiques' },
     ...thematics.map((item) => ({
       name: item.title,
       path: `/thematiques/${thematics.indexOf(item)}`,
       icon: item.icon,
     })),
-    { name: "Equipe pédagogique", path: "/equipe" },
-    { name: "Contact", path: "/contact" },
-  ];
+    { name: 'Equipe pédagogique', path: '/equipe' },
+    { name: 'Contact', path: '/contact' },
+  ]
 
   return (
     <Navbar
       onMenuOpenChange={setIsMenuOpen}
-      maxWidth='full'
-      className='pb-4 bg-navbar-bg dark:bg-dark-navbar-bg'
+      maxWidth="full"
+      className="pb-8 bg-navbar-bg h-[200px] sm:h-[150px] md:h-[200px]"
       classNames={{
         item: [
-          "flex",
-          "relative",
-          "h-full",
-          "items-center",
+          'flex',
+          'relative',
+          'h-full',
+          'items-center',
           "data-[active=true]:after:content-['']",
-          "data-[active=true]:after:absolute",
-          "data-[active=true]:after:bottom-0",
-          "data-[active=true]:after:left-0",
-          "data-[active=true]:after:right-0",
-          "data-[active=true]:after:h-[2px]",
-          "data-[active=true]:after:rounded-[2px]",
-          "data-[active=true]:after:bg-primary",
+          'data-[active=true]:after:absolute',
+          'data-[active=true]:after:bottom-0',
+          'data-[active=true]:after:left-0',
+          'data-[active=true]:after:right-0',
+          'data-[active=true]:after:h-[2px]',
+          'data-[active=true]:after:rounded-[2px]',
+          'data-[active=true]:after:bg-primary',
         ],
       }}
     >
-      <NavbarContent>
+      <picture className="h-[50px] sm:h-[150px] md:h-[200px] flex justify-center w-full md:w-auto ">
+        <Link href="/" aria-current="page">
+          <Image
+            src={whitelogo}
+            alt="Logo Activ Savoirs"
+            className="object-fill"
+            width={300}
+            height={50}
+          />
+        </Link>
         <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className='sm:hidden'
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
         />
+      </picture>
+      <NavbarContent className="hidden sm:block">
         <NavbarBrand>
-          <Link
-            href='/'
-            aria-current='page'
-            className='pt-2 md:mr-2 lg:mr-4 xl:mr-16'
-          >
-            <Image src={logo} alt='Logo Activ Savoirs' height={80} />
-          </Link>
-          <div className='hidden sm:block'>
+          <div className="hidden sm:block">
             <Dropdown>
-              <NavbarItem isActive={path.includes("/thematiques")}>
+              <NavbarItem isActive={path.includes('/thematiques')}>
                 <DropdownTrigger>
                   <Button
                     disableRipple
                     className={` ${
-                      path.includes("#")
-                        ? "font-bold sm:text-xs lg:text-base data-[hover=true]:bg-transparent"
-                        : "p-0 sm:text-xs lg:text-base data-[hover=true]:bg-transparent hover:text-gray-600"
+                      path.includes('#')
+                        ? 'font-bold sm:text-xs lg:text-base data-[hover=true]:bg-transparent'
+                        : 'p-0 sm:text-xs lg:text-base data-[hover=true]:bg-transparent hover:text-gray-600'
                     }`}
                     endContent={<FiChevronDown />}
-                    radius='sm'
-                    variant='light'
+                    radius="sm"
+                    variant="light"
                   >
-                    <span className='text-base'>Nos thématiques</span>
+                    <span className="text-xs sm:text-sm lg:text-base">
+                      Nos thématiques
+                    </span>
                   </Button>
                 </DropdownTrigger>
               </NavbarItem>
-              <DropdownMenu aria-label='Item_3' items={thematics}>
+              <DropdownMenu aria-label="Item_3" items={thematics}>
                 {(thematique: any) => {
                   return (
                     <DropdownItem
                       key={thematique.title}
-                      color={"undefined" as any}
-                      variant='shadow'
+                      color={'undefined' as any}
+                      variant="shadow"
                       className={`${
                         colorVariants[thematique.title]
                       } p-4 space-x-4`}
                       description={thematique.description}
                       startContent={thematique.icon}
-                      onClick={() => router.push(thematique.path || "#")}
+                      onClick={() => router.push(thematique.path || '#')}
                     >
                       {thematique.title}
                     </DropdownItem>
-                  );
+                  )
                 }}
               </DropdownMenu>
             </Dropdown>
@@ -124,23 +129,23 @@ export function Top() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className='hidden sm:flex' justify='end'>
-        <NavbarItem isActive={path.includes("/equipe")}>
+      <NavbarContent className="hidden sm:flex" justify="end">
+        <NavbarItem isActive={path.includes('/equipe')}>
           <Link
-            href='/equipe'
-            aria-current='page'
-            color='foreground'
-            className='hover:text-gray-600'
+            href="/equipe"
+            aria-current="page"
+            color="foreground"
+            className="hover:text-gray-600 text-xs sm:text-sm lg:text-base"
           >
             Equipe pédagogique
           </Link>
         </NavbarItem>
-        <NavbarItem isActive={path.includes("/contact")}>
+        <NavbarItem isActive={path.includes('/contact')}>
           <Link
-            href='/contact'
-            aria-current='page'
-            color='foreground'
-            className='px-8 border rounded-l-full rounded-r-full border-gray-950 py-2 hover:text-gray-600 hover:border-gray-600'
+            href="/contact"
+            aria-current="page"
+            color="foreground"
+            className="px-8 border rounded-l-full rounded-r-full border-gray-950 py-2 hover:text-gray-600 hover:border-gray-600 text-xs sm:text-sm lg:text-base"
           >
             Nous contacter
           </Link>
@@ -152,30 +157,30 @@ export function Top() {
           <NavbarMenuItem key={`${item}-${index}`}>
             {index === 1 && (
               <Link
-                color='primary'
-                className='hover:cursor-default font-bold ml-2'
-                size='lg'
+                color="primary"
+                className="hover:cursor-default font-bold ml-2"
+                size="lg"
               >
                 {item.name}
               </Link>
             )}
             {index >= 2 && index <= 10 && (
               <Link
-                color='foreground'
-                className='w-full hover:bg-rose-500 hover:text-white p-2 hover:rounded-xl hover:w-1/2 ml-4'
+                color="foreground"
+                className="w-full hover:bg-rose-500 hover:text-white p-2 hover:rounded-xl hover:w-1/2 ml-4"
                 href={item.path}
-                size='lg'
+                size="lg"
               >
-                <div className='mr-2'>{item.icon}</div>
+                <div className="mr-2">{item.icon}</div>
                 {item.name}
               </Link>
             )}
             {(index === 0 || index >= 11) && (
               <Link
-                color='foreground'
-                className='w-full hover:bg-rose-500 hover:text-white p-2 hover:rounded-xl hover:w-1/2'
+                color="foreground"
+                className="w-full hover:bg-rose-500 hover:text-white p-2 hover:rounded-xl hover:w-1/2"
                 href={item.path}
-                size='lg'
+                size="lg"
               >
                 {item.name}
               </Link>
@@ -184,5 +189,5 @@ export function Top() {
         ))}
       </NavbarMenu>
     </Navbar>
-  );
+  )
 }
