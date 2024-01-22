de6b4cc6b8245b133de03461415840278f893c3a
koala819 <x.genolhac@gmail.com>
2024-01-22 15:20:14 +0100

    remove text over YT video

--- src/components/molecules/YouTube.tsx
+++ src/components/molecules/YouTube.tsx
@@ -1,14 +1,17 @@
-"use client";
-import ReactPlayer from "react-player";
-import Image from "next/image";
-import dataDock from "@/public/images/dataDock.png";
-import { WhiteBox } from "@/src/components/atoms/WhiteBox";
+'use client'
+
+import ReactPlayer from 'react-player'
+
+import Image from 'next/image'
+
+// import { WhiteBox } from '@/src/components/atoms/WhiteBox'
+import dataDock from '@/public/images/dataDock.png'
 
 export function YouTube({ video }: { video: any }) {
   return (
-    <section className='flex justify-center items-center'>
-      <div className='w-2/3 sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 relative h-[400px] rounded-xl'>
-        <WhiteBox
+    <section className="flex justify-center items-center">
+      <div className="w-2/3 sm:w-1/2 md:w-2/3 lg:w-1/2 xl:w-1/3 relative h-[400px] rounded-xl">
+        {/* <WhiteBox
           number={4850}
           text='Elèves formés'
           className='top-28 -left-16'
@@ -17,24 +20,24 @@ export function YouTube({ video }: { video: any }) {
           number={2368}
           text='Formations'
           className='top-8 -right-8 sm:-right-16'
-        />
+        /> */}
 
-        <div className='w-full h-full absolute'>
+        <div className="w-full h-full absolute">
           <ReactPlayer
             controls={true}
-            url={video.data.video.html || ""}
-            width='100%'
-            height='100%'
+            url={video.data.video.html || ''}
+            width="100%"
+            height="100%"
           />
         </div>
         <Image
           src={dataDock}
-          alt='logo dataDock'
-          className='absolute -bottom-4 md:-bottom-8 -right-16 '
+          alt="logo dataDock"
+          className="absolute -bottom-4 md:-bottom-8 -right-16 "
           width={50}
           height={100}
         />
       </div>
     </section>
-  );
+  )
 }
